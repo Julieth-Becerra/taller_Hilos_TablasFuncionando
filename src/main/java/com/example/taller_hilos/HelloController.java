d304a0f17c3ad5badb5194ecb10ac17797c41f57
Julieth-Becerra <julieth.becerra01@uptc.edu.co>
2022-02-27 11:14:28 -0500

    Terminado :)

--- src/main/java/com/example/taller_hilos/HelloController.java
+++ src/main/java/com/example/taller_hilos/HelloController.java
@@ -1,11 +1,10 @@
 package com.example.taller_hilos;
 
 import Model.Person;
+import Model.Persona;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.Label;
@@ -61,12 +60,81 @@ public class HelloController implements Initializable {
     private AnchorPane turnos;
 
 
+    @FXML
+    private TableView<Persona> tablaA;
+    @FXML
+    private TableColumn AName;
+    @FXML
+    private TableColumn AId;
+    @FXML
+    private TableColumn ATurno;
+    ObservableList<Persona> personasTablaA;
+
+
+    @FXML
+    private TableView<Persona> tablaC;
+    @FXML
+    private TableColumn CName;
+    @FXML
+    private TableColumn CId;
+    @FXML
+    private TableColumn CTurn;
+    ObservableList<Persona> personasTablaC;
+
+
+
+    @FXML
+    private TableView<Persona> tablaAC;
+    @FXML
+    private TableColumn ACName;
+    @FXML
+    private TableColumn ACId;
+    @FXML
+    private TableColumn ACTurn;
+    ObservableList<Persona> personasTablaAC;
+
+
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
          //Se agregan los items al select
+
         boxServices.getItems().addAll(listServices);
         startThreads();
+
     }
+
+
+    private void inicializar_tabla (){
+        AName.setCellValueFactory(new PropertyValueFactory<Persona,String>("nombre"));
+        AId.setCellValueFactory(new PropertyValueFactory<Persona,String>("id"));
+        ATurno.setCellValueFactory(new PropertyValueFactory<Persona,String>("turno"));
+
+        personasTablaA = FXCollections.observableArrayList();
+        tablaA.setItems(personasTablaA);
+
+
+        CName.setCellValueFactory(new PropertyValueFactory<Persona,String>("nombre"));
+        CId.setCellValueFactory(new PropertyValueFactory<Persona,String>("id"));
+        CTurn.setCellValueFactory(new PropertyValueFactory<Persona,String>("turno"));
+
+        personasTablaC = FXCollections.observableArrayList();
+        tablaC.setItems(personasTablaC);
+
+
+
+
+        ACName.setCellValueFactory(new PropertyValueFactory<Persona,String>("nombre"));
+        ACId.setCellValueFactory(new PropertyValueFactory<Persona,String>("id"));
+        ACTurn.setCellValueFactory(new PropertyValueFactory<Persona,String>("turno"));
+
+        personasTablaAC = FXCollections.observableArrayList();
+        tablaAC.setItems(personasTablaAC);
+
+
+    }
+
 
     private void startThreads() {
         //Se colocan los label que dependen del estado
@@ -88,7 +156,8 @@ public class HelloController implements Initializable {
         }else {
             Person persona = new Person(this.txtName.getText(), this.txtId.getText(), this.lblTurnos.getText());
             persona.setModule(boxServices.getValue());
-            System.out.println(boxServices.getValue());
+
+
             switch (boxServices.getValue()) {
                 case "Autorizaciones" -> {
                     lblTurnos.setText("A" + lblAEstado.getSize());
@@ -100,6 +169,7 @@ public class HelloController implements Initializable {
                 }
                 case "Citas" -> {
                     lblTurnos.setText("C" + lblCEstado.getSize());
+
                     persona.asignTurn(lblTurnos.getText());
                     lblCEstado.addItem(persona);
                     personCList.add(new Person(txtName.getText(), txtId.getText(), lblTurnos.getText()));
@@ -122,16 +192,51 @@ public class HelloController implements Initializable {
     public void estadisticas (){
         estadisticas.setVisible(true);
         turnos.setVisible(false);
+
+
+
+        this.inicializar_tabla();
+
+
         for (int i = 0; i < personAList.size(); i++) {
 
-            System.out.println(personAList.get(i));
+
+            Persona persona = new Persona();
+            persona.nombre.set(personAList.get(i).getName());
+            persona.id.set(personAList.get(i).getId());
+            persona.turno.set(personAList.get(i).getTurn());
+
+            personasTablaA.add(persona);
+
+
         }
+
+
+
+
+
         for (int i = 0; i < personCList.size() ; i++) {
 
-            System.out.println(personCList.get(i));
+
+            Persona persona = new Persona();
+            persona.nombre.set(personCList.get(i).getName());
+            persona.id.set(personCList.get(i).getId());
+            persona.turno.set(personCList.get(i).getTurn());
+
+            personasTablaC.add(persona);
+
+
+
         }
         for (int i = 0; i < personACList.size(); i++) {
-            System.out.println(personACList.get(i));
+
+            Persona persona = new Persona();
+            persona.nombre.set(personACList.get(i).getName());
+            persona.id.set(personACList.get(i).getId());
+            persona.turno.set(personACList.get(i).getTurn());
+
+            personasTablaAC.add(persona);
+
         }
     }
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,6 @@
 module com.example.taller_hilos {
+
+
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
@@ -6,4 +8,7 @@ module com.example.taller_hilos {
 
     opens com.example.taller_hilos to javafx.fxml;
     exports com.example.taller_hilos;
+
+    opens Model ;
+    exports Model;
 }
